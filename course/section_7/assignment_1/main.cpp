c1415a6a6e56e7f4f35ceafdc13cafae2c108886
MHJMBS <mhjmbs@hotmail.com>
2023-05-28 16:37:40 -0300

    Add Section 7

---
+++ course/section_7/assignment_1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+    
+int main() {
+    
+    std::string country;
+    std::cout << "What country do you live in?" << std::endl;
+    std::cin >> country;
+    std::cout << "You live in " << country << "?! Me too!!!"<< std::endl;
+    
+}
