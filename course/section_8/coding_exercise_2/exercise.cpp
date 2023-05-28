dbbb8d6cad8c07f674896e04691890c8d626edd5
MHJMBS <mhjmbs@hotmail.com>
2023-05-28 16:37:58 -0300

    Add Section 8

---
+++ course/section_8/coding_exercise_2/exercise.cpp
@@ -0,0 +1,16 @@
+#ifndef EXERCISE_H
+#define EXERCISE_H
+
+double volume(){
+    
+    //Your code will go below this line
+
+    double width = 10;
+    double length = 20.1;
+    double height = 4.5;
+    
+    //Your code will go above this line
+    return width * length * height;
+}
+
+#endif // _EXERCISE_H
