ed551cf74f48a19dc25d9f170c2bf02fbb6d31b1
MHJMBS <mhjmbs@hotmail.com>
2023-06-02 21:22:25 -0300

    Add Section 35

---
+++ course/section_35-namespaces/coding_exercise_64/exercise.cpp
@@ -0,0 +1,8 @@
+#include "exercise.h"
+#include <iostream>
+
+namespace Utilities {
+    void print_point(Geom::Point& p){
+	    std::cout << "Point[x : " << p.x() << ", y : " << p.y() << "]" ;
+    }
+}
