5a574c9868e63fa6e1ff26a6e46245892c1a6df1
MHJMBS <mhjmbs@hotmail.com>
2023-06-09 13:26:13 -0300

    Add Section 40

---
+++ course/section_40-inheritance/coding_exercise_82/exercise.h
@@ -0,0 +1,27 @@
+#ifndef EXERCISE_H
+#define EXERCISE_H
+#include <iostream>
+
+class A{
+	protected :
+	 int m_x;
+};
+
+class B : public A{
+	public : 
+		int m_y;
+		void set_values(int x, int y, int z){
+			m_x = x;
+			m_y = y;
+			m_z = z;
+		}
+		void print() const{
+			std::cout << "x : " << m_x << ",y : " << m_y
+				<< ",z : " << m_z ;
+		}
+	private : 
+		int m_z;
+};
+
+
+#endif // _EXERCISE_H
